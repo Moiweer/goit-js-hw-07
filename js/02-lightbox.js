d117e6ba22e30ae5280444bd889d4c742bea8e28
Mariya Barska <moiweer@gmail.com>
2022-07-29 22:53:40 +0300

    ++

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery")
+
+const createGalleryCardsMarkup = items => items.map(
+    ({ preview, original, description }) =>
+        `
+   <a class="gallery__item" href="${original}">
+   <img class="gallery__image"
+        src="${preview}"
+        alt="${description}"/>
+    </a>
+    `
+).join('');
+   
+
+    
+const galleryCards = createGalleryCardsMarkup(galleryItems)
+
+gallery.insertAdjacentHTML('beforeend', galleryCards);
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    animationSpeed: 250,
+});
